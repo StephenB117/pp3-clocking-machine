e46902fa836fff993fc1a811382d59fa7f2daf7c
Stephen Bateson <stephen.lambkin@gmail.com>
2024-05-30 17:01:32 +0000

    Added function to validate user input data

--- run.py
+++ run.py
@@ -14,5 +14,29 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("pp3-clocking-machine")
 
-print("Please enter your clocking number or enter 9999 to register as a new user")
+def get_badge_number():
+    """
+    Gets the users badge number from the user via the terminal.
+    Badge number must only contain numbers in the range of 0001 to 9998.  
+    """
+    badge = input("Please enter your clocking number or enter 9999 to register as a new user\n")
 
+    validate_user_input(badge)
+
+def validate_user_input(data):
+    """
+    Inside the try, converts all string values into integers,
+    Raises ValueError is strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(nums) for nums in data]
+        if len(data) != 4:
+            raise ValueError(f"Badge number must be 4 numbers you have entered {len(data)}")
+
+    except ValueError as e:
+        print(f"invalid data: {e} please enter a number\n")
+
+    return data 
+   
+get_badge_number()
