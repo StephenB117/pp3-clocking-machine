b4c45818f137db231f20ae4cc854eb1e6bf0a255
Stephen Bateson <stephen.lambkin@gmail.com>
2024-05-30 18:52:54 +0000

    added function to check if a new or current user is using the system

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def get_badge_number():
     while True:
         badge = input("Please enter your clocking number or enter 9999 to register as a new user\n")
         if validate_user_input(badge):
-            print("Badge valid")
+            print("Input valid")
             break
     return badge
 
@@ -30,7 +30,7 @@ def validate_user_input(data):
     """
     Inside the try, converts all string values into integers,
     Raises ValueError is strings cannot be converted into int,
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 4 values.
     """
     try:
         [int(nums) for nums in data]
@@ -43,8 +43,38 @@ def validate_user_input(data):
 
     return True 
 
+def new_or_current_user(data):
+    """
+    Checks if the user has entered 9999 to register as a new user. Or if they are a current user.
+    If they are a new user they are asked to enter their desired badge number. 
+    If they are a returning user they are checked against the google sheet. 
+    """
+    current_badges = SHEET.worksheet("badge_numbers").col_values(1)
+    if data == "9999":
+        new_badge = input("Please enter your desired badge number\n")
+
+        while True:
+            if new_badge in current_badges:
+                print("This badge is current unavailable please try again\n")
+                new_badge = input("Please enter your desired badge number\n")
+            else:
+                print(f"This badge is available. Your new badge number is {new_badge}\n")
+                SHEET.worksheet("badge_numbers").append_row([new_badge])
+                print("Badge number added to database\n")
+                break
+
+    else:
+        while True:
+            if data in current_badges:
+                print("Welcome back\n")
+                break
+            else:
+                print("Badge number does not exist\n")
+                data = input("Please enter your badge number again\n")
+
 
 
 
 
 test = get_badge_number()
+new_or_current_user(test)
